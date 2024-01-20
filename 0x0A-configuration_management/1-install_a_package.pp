f0dd850d26d5cd7f778b1bc2c0abd19760378d4e
soufianebibeche1 <soufianeloco98@gmail.com>
2024-01-20 12:22:45 -0800

    add more

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# Installs a package puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# File: 1-install_a_package.pp
+# Install Flask package using pip3
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
