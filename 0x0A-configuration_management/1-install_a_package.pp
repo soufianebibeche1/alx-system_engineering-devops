9f04808e0e1e6006e0ae599d9aa5a13a33bc7ed5
soufianebibeche1 <soufianeloco98@gmail.com>
2024-01-20 12:20:06 -0800

    more fixe

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# A manifest that installs flask from pip3
-
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Installs a package puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
