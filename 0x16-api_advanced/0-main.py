66ba6bc0a75f598ed95e442729dd057642c45e40
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-03-05 22:17:15 +0000

    testera

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""
+0-main
+"""
 import sys
-from subs import number_of_subscribers
 
 if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        subreddit = sys.argv[1]
-        print("{:d}".format(number_of_subscribers(subreddit)))
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
