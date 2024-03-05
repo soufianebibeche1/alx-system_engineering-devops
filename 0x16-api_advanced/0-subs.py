116c288091d3b41c0874937602a6c307d721e3ff
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-03-05 22:13:24 +0000

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,15 +2,15 @@
 """
 Script that queries subscribers on a given Reddit subreddit.
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {'User-Agent': 'My User Agent'}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,44 +1,22 @@
 #!/usr/bin/python3
-"""
-Retrieve top ten posts from a given subreddit.
+"""API function that queries the Reddit API and prints
+the titles of the first 10 hot posts listed for a given
+subreddit
 """
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """
-    Print the titles of the first 10 hot posts listed for a given subreddit.
+    """Print the titles of the first 10 hot posts
+    listed for a given subreddit"""
 
-    :param subreddit: The subreddit name.
-    """
     url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an exception for non-200 status codes
-
-        data = response.json().get("data")
-        if data:
-            children = data.get("children")
-            if children:
-                for post in children:
-                    post_details = post.get("data")
-                    print(post_details.get("title"))
-    except requests.HTTPError as e:
-        if e.response.status_code == 404:
-            print(None)
-        elif e.response.status_code == 403:
-            print("Access to the subreddit is forbidden. Please check your permissions.")
-        else:
-            print("Error occurred:", e)
-    except Exception as e:
-        print("An unexpected error occurred:", e)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+    resp = requests.get(url=url, headers=headers)
+    if resp.status_code == 404:
+        print(None)
+        return
+    subs = resp.json().get("data")
+    for post in subs.get("children"):
+        post_details = post.get("data")
+        print(post_details.get("title"))
