a45d400bfa04de225454543123678f06d554315a
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-03-05 21:46:33 +0000

    testera

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 """
-Get the number of subscribers for a given subreddit.
+Script that queries subscribers on a given Reddit subreddit.
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Print the titles of the first 10 hot posts
-    listed for a given subreddit"""
-    headers = {'User-Agent': 'My User Agent'}
-    
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers)
-    
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
     else:
         return 0
-
-if __name__ == '__main__':
-    subreddit = input("Enter the subreddit name: ")
-    print("{:d}".format(number_of_subscribers(subreddit)))
